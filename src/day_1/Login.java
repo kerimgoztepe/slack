a720d5df50a17add3c9a379c18424e4dfceb9080
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-12 19:24:26 +0300

    new file

---
+++ src/day_1/Login.java
@@ -0,0 +1,12 @@
+package day_1;
+
+public class Login {
+    public static void main(String[] args) {
+        String userName = "JohnIsaac";
+        String  passWd = "wert1234";
+        System.out.println("Name is ="+userName);
+        System.out.println("passWd ="+passWd);
+
+
+    }
+}
