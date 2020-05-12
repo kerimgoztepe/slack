7461fbb153725b1bb9d7b6eaea0595059995f37e
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-12 18:51:29 +0300

    new file

---
+++ src/day_1/CreateChannel.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class CreateChannel {
+    public static void main(String[] args) {
+        String name = "John Isaac Berger";
+        System.out.println("name = "+name);
+    }
+
+
+}
