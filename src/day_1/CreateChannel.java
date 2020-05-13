d708c76deec0a816bc36519420b2e8b6d0d5f996
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-13 17:56:12 +0300

    new codes in createchannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,6 +4,10 @@ public class CreateChannel {
     public static void main(String[] args) {
         String name = "John Isaac Berger";
         System.out.println("name = "+name);
+
+        int a = 23, b = 45, c =36;
+        int sum = a+b+c;
+        System.out.println("Sum is here =" +sum);
     }
 
 
