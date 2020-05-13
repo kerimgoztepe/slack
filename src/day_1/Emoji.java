c49ddf0405823a73ebe4fae8053486c73133c5db
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-13 19:06:50 +0300

    added emoji

---
+++ src/day_1/Emoji.java
@@ -0,0 +1,13 @@
+package day_1;
+
+public class Emoji {
+
+    public static void main(String[] args) {
+        String name = "John Isaac Berger";
+        System.out.println("Name is = "+name);
+        System.out.println("This is class emoji = ");
+
+
+    }
+
+}
